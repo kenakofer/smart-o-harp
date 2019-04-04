8a68e3db3914f3271c9b473c8b5cc78c8b6d87bf
Kenan Bitikofer <kbitikofer@gmail.com>
2019-04-03 21:07:43 -0400

    Get code to compile

--- code/code.ino
+++ code/code.ino
@@ -2,7 +2,7 @@ int current_key = 0; // The key the instrument is currently in, with C being 0
 
 void change_key_by(int half_steps) {
     current_key = current_key + half_steps % 12;
-{
+}
 
 // Just the basic chords. Modifiers, including minor should all be applied as functions
 // -1 indicates no note
@@ -32,58 +32,58 @@ const int VII[] = {11, 3, 6, -1};
 int current_chord[] = {-1, -1, -1, -1};
 
 // Copy the specified triad into the current_chord
-void set_current_chord(int[] triad) {
+void set_current_chord(int triad[]) {
     current_chord[0] = (triad[0] + current_key) % 12;
     current_chord[1] = (triad[1] + current_key) % 12;
     current_chord[2] = (triad[2] + current_key) % 12;
-    current_chord[3] = (triad[3] + current_key) % 12;
 }
 
 // All the modifiers, change the chord in place
-void make_sus2(int[] chord) {
+void make_sus2(int chord[]) {
     chord[1] = (chord[0] + 2) % 12;
     //chord.name = String(chord.name + "_sus2")
 }
-void make_minor(int[] chord) {
+void make_minor(int chord[]) {
     chord[1] = (chord[0] + 3) % 12;
     //chord.name = String(chord.name + "_min")
 }
-void make_major(int[] chord) {
+void make_major(int chord[]) {
     chord[1] = (chord[0] + 4) % 12;
     //chord.name = String(chord.name + "_maj")
 }
-void make_sus4(int[] chord) {
+void make_sus4(int chord[]) {
     chord[1] = (chord[0] + 5) % 12;
     //chord.name = String(chord.name + "_sus4")
 }
-void make_dim(int[] chord) {
+void make_dim(int chord[]) {
     chord[1] = (chord[0] + 3) % 12;
     chord[2] = (chord[0] + 6) % 12;
     //chord.name = String(chord.name + "_dim")
 }
-void make_aug(int[] chord) {
+void make_aug(int chord[]) {
     chord[1] = (chord[0] + 4) % 12;
     chord[2] = (chord[0] + 8) % 12;
     //chord.name = String(chord.name + "_aug")
 }
-void add_dim7(int[] chord) {
+void add_dim7(int chord[]) {
     chord[3] = (chord[0] + 9) % 12;
     //chord.name = String(chord.name + "_dim7")
 }
-void add_min7(int[] chord) {
+void add_min7(int chord[]) {
     chord[3] = (chord[0] + 10) % 12;
     //chord.name = String(chord.name + "_min7")
 }
-void add_maj7(int[] chord) {
+void add_maj7(int chord[]) {
     chord[3] = (chord[0] + 11) % 12;
     //chord.name = String(chord.name + "_maj7")
 }
 
 
-const int[] col_pin_numbers = { -1, -1, -1 };
-const int[] col_pin_states   = { -1, -1, -1 };
-const int[] row_pin_numbers = { -1, -1, -1, -1 };
-const int[] row_pin_states   = { -1, -1, -1, -1 };
+const int col_pin_numbers[] = { 4, 5, 6 };
+int col_pin_states[]   = { -1, -1, -1 };
+
+const int row_pin_numbers[] = { 0, 1, 2, 3 };
+int row_pin_states[]   = { -1, -1, -1, -1 };
 
 void setup() {
     Serial.begin(9600);
@@ -97,22 +97,26 @@ void setup() {
 
 void loop() {
 
+    delay(500);
+
     // Read all the pins once
     for (int i=0; i< sizeof(col_pin_numbers); i++) {
-        col_pin_states[i] = digitalRead(col_pin_numbers[i])
+        col_pin_states[i] = digitalRead(col_pin_numbers[i]);
     }
     for (int i=0; i< sizeof(row_pin_numbers); i++) {
-        row_pin_states[i] = digitalRead(row_pin_numbers[i])
+        row_pin_states[i] = digitalRead(row_pin_numbers[i]);
     }
 
     // Debug print the buttons pushed
-    for (int r=0; r< sizeof(row_pin_states); r++) {
-        for (int c=0; c< sizeof(col_pin_states); c++) {
+    for (int r=0; r<sizeof(row_pin_states); r++) {
+        for (int c=0; c<sizeof(col_pin_states); c++) {
             if (row_pin_states[r] == LOW && col_pin_states[c] == LOW) {
-                Serial.print("1")
+                Serial.print("1");
             } else {
-                Serial.print("0")
+                Serial.print("0");
             }
         }
+        Serial.println();
     }
+    Serial.println();
 }
